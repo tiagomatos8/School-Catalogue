30067ce37729c47dce6dcba5e5740fbe88b73777
Tiago C M Matos <tcm.matos@gmail.com>
2018-04-15 22:48:56 +1000

    First commit

---
+++ SchoolCatalogue.js
@@ -0,0 +1,75 @@
+class School {
+    constructor(name, level, numberOfStudents) {
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+
+    get name() {
+        return this._name;
+    }
+    get level() {
+        return this._level;
+    }
+    get numberOfStudents() {
+        return this._numberOfStudents;
+    }
+    set numberOfStudents(newNumberOfStudents) {
+        if (typeof newNumberOfStudents === 'number') {
+            this._numberOfStudents = newNumberOfStudents;
+        } else {
+            console.log('Invalid input: numberOfStudents must be set to a Number.');
+        }
+    }
+
+    quickFacts() {
+        console.log(
+            `${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`
+        );
+    }
+
+    static pickSubstituteTeacher(substituteTeachers) {
+        const randomTeacher = Math.floor(Math.random() * substituteTeachers.length);
+        return substituteTeachers[randomTeacher];
+    }
+}
+
+class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickupPolicy) {
+        super(name, 'primary', numberOfStudents);
+        this._pickupPolicy = pickupPolicy;
+    }
+
+    get pickupPolicy() {
+        return this._pickupPolicy;
+    }
+}
+
+class MiddleSchool extends School {
+    constructor(name, numberOfStudents) {
+        super(name, level, numberOfStudents);
+    }
+}
+
+class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+        super(name, 'high', numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
+
+    get sportsTeams() {
+        this._sportsTeams.forEach(element => {
+            console.log(element);
+        });
+    }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 
+                    'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+
+lorraineHansbury.quickFacts();
+const teachers = ['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli'];
+console.log(PrimarySchool.pickSubstituteTeacher(teachers));
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+alSmith.sportsTeams;
